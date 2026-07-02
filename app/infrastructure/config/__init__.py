316b0e056e0fa3872243f81a534b64bfd9f9c6ad
Gr8Soln <emmanuelisholaayinde@gmail.com>
2026-07-02 15:14:20 +0100

    Refactor code structure for improved readability and maintainability

---
+++ app/infrastructure/config/__init__.py
@@ -0,0 +1,3 @@
+from app.infrastructure.config.settings import *     
+from app.infrastructure.config.logging import *
+from app.infrastructure.config.normalizer import *
--- app/infrastructure/config/settings.py
+++ app/infrastructure/config/settings.py
@@ -81,6 +81,3 @@ class Settings(BaseSettings):
 
 def get_settings() -> Settings:
     return Settings()  # type: ignore[call-arg]
-
-
-settings = get_settings()
